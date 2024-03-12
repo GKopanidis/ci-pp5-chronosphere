0e0a80deba5d3fdb3bafe026eaf6cbb374521ea9
Georgios Kopanidis <kopanidis@me.com>
2024-03-12 08:43:25 +0000

    Add likes functionality

--- likes/models.py
+++ likes/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
-# Create your models here.
-
 
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from chronosphere_drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
 # Create your views here.
+
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
