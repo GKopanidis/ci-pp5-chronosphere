f4ea9cff84c04b5a8059441ce7013de25d703849
Georgios Kopanidis <kopanidis@me.com>
2024-03-12 10:14:47 +0000

    Add serializers and authentication settings

--- chronosphere_drf_api/settings.py
+++ chronosphere_drf_api/settings.py
@@ -55,12 +55,39 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'profiles',
     'posts',
     'comments',
     'likes',
     'followers',
 ]
+
+SITE_ID = 1
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'chronosphere_drf_api.serializers.CurrentUserSerializer'
+}
+
 
 cloudinary.config(
     secure=True
