048ef3f78a620637421bb7a3e7ddce8b42c5b4b5
Georgios Kopanidis <kopanidis@me.com>
2024-03-12 08:09:32 +0000

    Refactor CommentSerializer and add CommentDetailSerializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,10 +3,6 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Comment model
-    Adds three extra fields when returning a list of Comment instances
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -22,3 +18,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
