d5a1cbd1f7dc85fefefdc6b34f299d8bdf8b0dba
Georgios Kopanidis <kopanidis@me.com>
2024-03-12 09:57:42 +0000

    Add DjangoFilterBackend to CommentList view and update filterset_fields

--- profiles/views.py
+++ profiles/views.py
@@ -20,7 +20,7 @@ class ProfileList(generics.ListAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'posts_count',
