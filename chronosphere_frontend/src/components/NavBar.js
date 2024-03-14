91f339b200b28f64884f0bd696b8ec34585a88d7
Georgios Kopanidis <kopanidis@me.com>
2024-03-14 15:00:50 +0000

    Update favicon and logo paths, add font stylesheet, and refactor App component

--- chronosphere_frontend/src/components/NavBar.js
+++ chronosphere_frontend/src/components/NavBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/chronosphere_logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="60" />
+          <img src={logo} alt="logo" height="60" />{" "}
+          <span className={styles.chrono}>Chrono</span>
+          <span className={styles.sphere}>Sphere</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
