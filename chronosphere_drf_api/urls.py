1611f35b94c532989edcdd67ddd96da70512c94f
Georgios Kopanidis <kopanidis@me.com>
2024-03-14 16:18:57 +0000

    Add cloudinary_storage and whitenoise middleware, update static file settings, and update API URLs

--- chronosphere_drf_api/urls.py
+++ chronosphere_drf_api/urls.py
@@ -15,20 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route, logout_route
+from django.views.generic import TemplateView
+from .views import logout_route
 
 urlpatterns = [
-    path('', root_route),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('followers.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('posts.urls')),
+    path('api/', include('comments.urls')),
+    path('api/', include('likes.urls')),
+    path('api/', include('followers.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
