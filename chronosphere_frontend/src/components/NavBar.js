fc13df8797b479089a2f1a7b0f8a1e8c8fccd0e2
Georgios Kopanidis <kopanidis@me.com>
2024-03-14 14:22:35 +0000

    Update favicon and add NavBar component

---
+++ chronosphere_frontend/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/chronosphere_logo.png";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="60" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-house-chimney"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-right-to-bracket"></i>Sign In
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-user-plus"></i>Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
