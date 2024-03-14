49c892e4cee1113b188aaead93c89d850af52eef
Georgios Kopanidis <kopanidis@me.com>
2024-03-14 15:48:34 +0000

    Update styles and routes in NavBar and App components

--- chronosphere_frontend/src/components/NavBar.js
+++ chronosphere_frontend/src/components/NavBar.js
@@ -2,28 +2,44 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/chronosphere_logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="60" />{" "}
-          <span className={styles.chrono}>Chrono</span>
-          <span className={styles.sphere}>Sphere</span>
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="60" />{" "}
+            <span className={styles.chrono}>Chrono</span>
+            <span className={styles.sphere}>Sphere</span>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i className="fas fa-house-chimney"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i className="fa-solid fa-right-to-bracket"></i>Sign In
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
               <i className="fa-solid fa-user-plus"></i>Sign Up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
