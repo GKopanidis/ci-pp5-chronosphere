c38bd2b08879a056a7328d6b59301f672bb27eec
Georgios Kopanidis <kopanidis@me.com>
2024-03-19 12:07:13 +0000

    Add extra content to Asset component and link to home page in NotFound component

--- chronosphere_frontend/src/components/Asset.js
+++ chronosphere_frontend/src/components/Asset.js
@@ -2,12 +2,13 @@ import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 import styles from "../styles/Asset.module.css";
 
-const Asset = ({ spinner, src, message }) => {
+const Asset = ({ spinner, src, message, extraContent }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
       {spinner && <Spinner animation="border" />}
-      {src && <img src={src} alt={message} />}
+      {src && <img src={src} alt={message} className="mt-3" />}
       {message && <p className="mt-4">{message}</p>}
+      {extraContent}
     </div>
   );
 };
--- chronosphere_frontend/src/components/NotFound.js
+++ chronosphere_frontend/src/components/NotFound.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NoResults from "../assets/no-results.png";
 import styles from "../styles/NotFound.module.css";
 import Asset from "./Asset";
 
 const NotFound = () => {
+  const extraContent = (
+    <div className={NotFound.linkToHome}>
+      <Link to="/">Click here to go back to Home Page</Link>
+    </div>
+  );
+
   return (
     <div className={styles.NotFound}>
       <Asset
         src={NoResults}
-        message={`Sorry, the page you're looking for doesn't exist`}
+        message="Sorry, the page you're looking for doesn't exist"
+        extraContent={extraContent}
       />
     </div>
   );
