d8976d046926111cda3cc38136d232d7ac7f0818
Georgios Kopanidis <kopanidis@me.com>
2024-03-26 11:25:41 +0000

    Fix API endpoint in PostPage.js

--- chronosphere_frontend/src/pages/posts/PostPage.js
+++ chronosphere_frontend/src/pages/posts/PostPage.js
@@ -27,7 +27,7 @@ function PostPage() {
     const handleMount = async () => {
       try {
         const [{ data: post }, { data: comments }] = await Promise.all([
-          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/posts/${id}/`),
           axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
